06551b179c1d3485795bba59400ba4f16998f1c3
kevin-ranpura <kevinranpura27@gmail.com>
2025-07-27 00:41:22 +0530

    Final Commit

--- frontend/src/components/AddResume.jsx
+++ frontend/src/components/AddResume.jsx
@@ -2,16 +2,13 @@ import React from 'react'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useUser } from '../context/UserContext'
-import { getAuth } from "firebase/auth";
 
 
 const AddResume = () => {
-
   const navigate = useNavigate()
   const { user } = useUser()
-  const auth = getAuth()
   const primaryemail = user?.email
-  const username = auth.currentUser.displayName
+  const username = user?.displayName
   const [modal, setmodal] = useState(false)
   const [title, settitle] = useState("")
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,39 +1,53 @@
-import React from 'react'
-import { Link, Navigate, useLocation} from 'react-router-dom'
+import { React, useState } from 'react'
+import { Link, Navigate, useLocation } from 'react-router-dom'
 import { useUser } from '../context/UserContext'
-import {auth} from '../firebase.js'
+import { getAuth } from "firebase/auth";
 
 const Navbar = () => {
-    const {user} = useUser()
-    const location = useLocation()
-    const isEditPage = location.pathname.includes("/edit/");
+	const [showsignout, setshowsignout] = useState(false)
+	const { user } = useUser()
+	const auth = getAuth()
+	const username = user?.displayName
+	const email = user?.email
+	const location = useLocation()
+	const isEditPage = location.pathname.includes("/edit/");
 
-  return (
-    <div className='flex justify-between items-center px-5 md:px-10 py-3'>
-      <Link href={"/"}>
-        <p className={`${isEditPage ? "text-black" : "text-white"} text-xl md:text-2xl font-semibold`}>ResumeWiz </p>
-      </Link>
+	return (
+		<div className='relative flex justify-between items-center px-5 md:px-10 py-3'>
+			<Link href={"/"}>
+				<p className={`${isEditPage ? "text-black" : "text-white"} text-xl md:text-2xl font-semibold`}>ResumeWiz</p>
+			</Link>
 
-      {user ?
-        <div className='flex gap-5 items-center justify-center'>
-            <Link to={"/dashboard"}>
-                <button className={`${isEditPage? "bg-[#00133b] text-white" : "bg-blue-300 text-[#00133b]"} font-semibold rounded-md px-3 py-2 cursor-pointer`}>Dashboard</button>
-            </Link>  
-            <button
-            onClick={() => auth.signOut()}
-            className="bg-red-500 text-white px-3 py-2"
-            >
-            Sign Out
-            </button>
-        </div>:
-        <Link to={"auth/sign-in/"}>
-            <button className='bg-blue-300 text-[#00133b] font-semibold rounded-md px-3 py-2 cursor-pointer'>Get Started</button>
-        </Link>
-      }
-        
-    </div>
-    
-  )
+			{user ?
+				<div className='flex gap-5 items-center justify-center'>
+					<Link to={"/dashboard"}>
+						<button className={`${isEditPage ? "bg-[#00133b] text-white" : "bg-blue-300 text-[#00133b]"} font-semibold rounded-md px-3 py-2 cursor-pointer`}>Dashboard</button>
+					</Link>
+
+					<button onClick={() => setshowsignout(!showsignout)} id="avatarButton" type="button" data-dropdown-toggle="userDropdown" data-dropdown-placement="bottom-start" className={`${isEditPage ? "bg-[#00133b] text-white" : "bg-blue-300 text-[#00133b]"} w-10 h-10 rounded-full font-semibold cursor-pointer`}>{username.charAt(0)}
+					</button>
+
+					<div id="userDropdown" className={`${showsignout ? "" : "hidden"} absolute top-15 z-10 bg-white divide-y divide-gray-400 rounded-lg shadow-sm font-semibold w-50 text-[#00133b]`}>
+						<div className="px-2 py-3 text-sm text--900 ">
+							<div>{username}</div>
+							<div className="font-semibold truncate">{email}</div>
+						</div>
+						<div className="py-1">
+							<div onClick={() => auth.signOut()} className="px-2 py-2 text-sm text-red-700 hover:bg-gray-100 flex gap-2 cursor-pointer">
+								<img src="/signout.svg" alt="" />
+								Sign out
+							</div>
+						</div>
+					</div>
+				</div> :
+				<Link to={"auth/sign-in/"}>
+					<button className='bg-blue-300 text-[#00133b] font-semibold rounded-md px-3 py-2 cursor-pointer'>Get Started</button>
+				</Link>
+			}
+
+		</div>
+
+	)
 }
 
 export default Navbar
